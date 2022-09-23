542503abc4d1d2bfba4da9d417d043cfe7fd0210
Federico Barbieri <federicobarbieri54@gmail.com>
2022-09-23 22:34:56 +0200

    new

--- app.js
+++ app.js
@@ -132,33 +132,88 @@ const firstScreen = document.querySelector('#first-screen');
 
 let textH1 = document.querySelector('h1');
 
+// get size of device
+
+let clientWidth = document.getElementById('container').clientWidth;
+
+
 function changeScreen1 () {
+    
+
+    // if screen is smaller than 1000px then stick to the vertical imgs
+
+    if(clientWidth <= 1000){
     firstScreen.style.background = "url('quality/success.webp')";
     firstScreen.style.backgroundPosition = "center";
     firstScreen.style.backgroundSize = "cover";
     textH1.textContent = "SUCCESS AS A WAY OF LIVING";
+    } else if (clientWidth >= 1000) {
+
+    // if desktop, use horizontal
+    
+    firstScreen.style.background = "url('quality-desktop/desktop-bikini.webp')";
+    firstScreen.style.backgroundPosition = "center";
+    firstScreen.style.backgroundSize = "contain";
+    firstScreen.style.backgroundRepeat = "no-repeat";
+    textH1.textContent = "SUCCESS AS A WAY OF LIVING";
+    }
 }
 
 
 function changeScreen2 () {
+
+    if(clientWidth <= 1000){
     firstScreen.style.background = "url('quality/junior.webp')";
     firstScreen.style.backgroundPosition = "center";
     firstScreen.style.backgroundSize = "cover";
     textH1.textContent = "FIRST STEPS ARE THE SHINIEST";
-}
+    } else if (clientWidth >= 1000) {
+
+        // if desktop, use horizontal
+        
+        firstScreen.style.background = "url('quality-desktop/desktop-bikini2.webp')";
+        firstScreen.style.backgroundPosition = "center";
+        firstScreen.style.backgroundSize = "contain";
+        firstScreen.style.backgroundRepeat = "no-repeat";
+        textH1.textContent = "FIRST STEPS ARE THE SHINIEST";
+        }
+    }
+
 
 function changeScreen3 () {
+    if(clientWidth <= 1000){
     firstScreen.style.background = "url('quality/card.webp')";
     firstScreen.style.backgroundPosition = "center";
     firstScreen.style.backgroundSize = "cover";
     textH1.textContent = "BIG DREAMS AHEAD";
+    } else if (clientWidth >= 1000) {
+
+        // if desktop, use horizontal
+        
+        firstScreen.style.background = "url('quality-desktop/desktop-bikini3.webp')";
+        firstScreen.style.backgroundPosition = "center";
+        firstScreen.style.backgroundSize = "contain";
+        firstScreen.style.backgroundRepeat = "no-repeat";
+        textH1.textContent = "BIG DREAMS AHEAD";
+        }
 }
 
 function changeScreen4 () {
+    if(clientWidth <= 1000){
     firstScreen.style.background = "url('quality/milestone.webp')";
     firstScreen.style.backgroundPosition = "center";
     firstScreen.style.backgroundSize = "cover";
     textH1.textContent = "REACH YOUR MILESTONE IN STYLE";
+    } else if (clientWidth >= 1000) {
+
+        // if desktop, use horizontal
+        
+        firstScreen.style.background = "url('quality-desktop/desktop-bikini4.webp')";
+        firstScreen.style.backgroundPosition = "center";
+        firstScreen.style.backgroundSize = "contain";
+        firstScreen.style.backgroundRepeat = "no-repeat";
+        textH1.textContent = "REACH YOUR MILESTONE IN STYLE";
+        }
 }
 
 function everything(){
